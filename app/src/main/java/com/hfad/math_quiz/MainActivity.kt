21ea7e398201b18c90102823ff5132d58df2a127
raj_d <raj.dua23@gmail.com>
2022-07-05 10:51:00 -0400

    Font End Ready

--- app/src/main/java/com/hfad/math_quiz/MainActivity.kt
+++ app/src/main/java/com/hfad/math_quiz/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.hfad.math_quiz
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.VirtualLayout
+import com.google.android.material.internal.FlowLayout
+import java.util.concurrent.Flow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (resources.configuration.orientation ==
+            Configuration.ORIENTATION_PORTRAIT) {
+
+
+            findViewById<androidx.constraintlayout.helper.widget.Flow>(R.id.flowButtons)
+                .setMaxElementsWrap(4)
+
+        }
+
+        if (resources.configuration.orientation ==
+                Configuration.ORIENTATION_LANDSCAPE) {
+
+            findViewById<androidx.constraintlayout.helper.widget.Flow>(R.id.flowButtons)
+                .setMaxElementsWrap(2)
+        }
+
     }
 }
